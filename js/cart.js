e29bda0e0bc677063f850b3dad16f578ddc9a098
matiasdelabandera <matiasdelabandera@gmail.com>
2022-10-16 17:33:15 -0300

    entrega 5
    ultimos toques

--- js/cart.js
+++ js/cart.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function(e){
     getJSONData(JSON).then(function(JSON){
         if(JSON.status === "ok"){
             showCart(JSON);
-            document.getElementById("count").addEventListener("click",dinamicSubTotal)
         }
     })
 })
@@ -13,23 +12,23 @@ document.addEventListener("DOMContentLoaded", function(e){
 function showCart(array){
     let info = array.data.articles
     let carritoHTML ="";
-         costo = parseInt (info[0].unitCost)
+    let costo = parseInt (info[0].unitCost)
     let unidades = parseInt (info[0].count)
-    subTotal = unidades*costo
-
+    let subTotal = unidades*costo
         carritoHTML += `
         <th scope="row">1</th>
         <td><img src="${info[0].image}" class="img-thumbnail" style="width: 80px;"></td>
         <td>${info[0].name}</td>
         <td>${info[0].currency+info[0].unitCost}</td>
-        <td><input type="number" id="count" name="count" min="1" max="5" value="${info[0].count}"></input></td>
+        <td><input type="number" id="count" name="cantidad" min="1" max="99" value="${info[0].count}" onchange="dinamicSubTotal(${info[0].unitCost})"></input></td>
         <td id="subTotalHTML">${info[0].currency+subTotal}</td>
         `
-    
     document.getElementById("cart").innerHTML += carritoHTML;
 }
 
-function dinamicSubTotal(){
-    let cantidad =parseInt(getElementById(count).value)
-    subTotal = costo * cantidad
+function dinamicSubTotal(item){
+    let cantidad = document.getElementById("count").value;
+    let costo = parseInt(item);
+    let subTotal2 = cantidad*costo;
+    document.getElementById("subTotalHTML").innerHTML = "USD"+subTotal2;
 }
